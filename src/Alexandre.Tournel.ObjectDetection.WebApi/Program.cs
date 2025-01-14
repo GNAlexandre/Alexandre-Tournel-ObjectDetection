be8e7070335a599a61e27d76fcd9930d8775a74a
Alexandre Tournel <alexandre.tournel@lacatholille.fr>
2025-01-14 09:54:46 +0100

    Commit Global.json

--- src/Alexandre.Tournel.ObjectDetection.WebApi/Program.cs
+++ src/Alexandre.Tournel.ObjectDetection.WebApi/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
